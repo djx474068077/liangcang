fc25c0c2039ace3c289f53d700f85ac6d56d3aad
djx474068077 <474068077@qq.com>
2017-09-14 12:09:03 +0800

    董金鑫 9-14 中午 跳转购物车页面成功

--- js/Cart.js
+++ js/Cart.js
@@ -3,8 +3,9 @@
  * 包括购物车中商品数量的加减、购物车的数据加载等
  */
 var oTable = document.getElementById('cart-table');
-
-myajax.get("http://h6.duchengjiu.top/shop/api_cart.php?token=11ccd2d1e107dfa97f5e20b629f6a552",{},function(error,responseText){
+var token = sessionStorage.token;
+console.log(token);
+myajax.get("http://h6.duchengjiu.top/shop/api_cart.php?token=" + token,{},function(error,responseText){
 	var json = JSON.parse(responseText);
 	var data = json.data;
 	for(var i=0;i<data.length;i++){
@@ -12,13 +13,12 @@ myajax.get("http://h6.duchengjiu.top/shop/api_cart.php?token=11ccd2d1e107dfa97f5
 		var goodsImg = data[i].goods_thumb;
 		var goodsNum = data[i].goods_number;
 		var goodsPrice = data[i].goods_price;
-		oTable.innerHTML += `<tr>
-			<th><input type="checkbox"/>选择</th>
-			<th><img src="${goodsImg}"/>${goodsName}</th>
-			<th>${goodsNum}</th>
-			<th>${goodsPrice}</th>
-			<th>${goodsNum}*${goodsPrice}</th>
-			<th><a href="">删除</a></th>
+		oTable.innerHTML += `<tr class="product">
+			<td><img src="${goodsImg}"/>${goodsName}</td>
+			<td>${goodsNum}</td>
+			<td>${goodsPrice}</td>
+			<td>${goodsNum}*${goodsPrice}</td>
+			<td><a href="">删除</a></td>
 		</tr>`;
 	}
 });
